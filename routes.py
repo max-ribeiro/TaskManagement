608e22be0f5436ad85db7b42d0091af5d45f0704
max-ribeiro <max_ribeiro@live.com>
2022-10-27 07:59:59 -0300

    feat: mongoDb configuration for task insertion and retrieving

--- routes.py
+++ routes.py
@@ -1,48 +1,65 @@
-from datetime import timedelta, date
-from collections import defaultdict
-from flask import Blueprint, render_template, request, url_for, redirect
+from datetime import timedelta, datetime
+import uuid
+from flask import Blueprint, render_template, request, url_for, redirect, current_app
 
 pages = Blueprint("tasks", __name__, template_folder="templates", static_folder="static")
-tasks = ["some task"]
-completions = defaultdict(list)
 
 # creates a global datesRange variable that can be accessed in all aplication templates
 @pages.context_processor
 def add_calc_date_range():
-    def dateRange(start: date):
+    def dateRange(start: datetime):
         print(start)
         dates = [start + timedelta(days=diff) for diff in range(-3, 4)]
         return dates
 
     return{"dateRange": dateRange}
 
+def today_at_midnight():
+    today = datetime.today()
+    return datetime(today.year, today.month, today.day)
+
 @pages.route("/")
 def index():
     dateString = request.args.get("date")
     if dateString:
-        selectedDate = date.fromisoformat(dateString)
+        selectedDate = datetime.fromisoformat(dateString)
     else:
-        selectedDate = date.today()
+        selectedDate = today_at_midnight()
+
+    tasksOnDate = current_app.db.tasks.find({"added":{"$lte": selectedDate}})
+    completions = [
+        task["task"]
+        for task in  current_app.db.completions.find({"date": selectedDate})
+    ]
+
     return render_template(
         "index.html",
-        tasks=tasks,
+        tasks=tasksOnDate,
         title="Task Manager - Home",
         selectedDate=selectedDate,
-        completions=completions[selectedDate]
+        completions=completions
     )
 
 @pages.route("/add", methods=["GET", "POST"])
 def add_task():
-    if request.method == "POST":
-        tasks.append(request.form.get("task"))
+    today = today_at_midnight()
 
-    return render_template("addtask.html", title="Task Manager - Add Task", selectedDate=date.today())
+    if request.method == "POST":
+        task = request.form.get("task")
+        current_app.db.tasks.insert_one({
+            "_id": uuid.uuid4().hex,
+            "added": today,
+            "name": task
+        })
+
+    return render_template("addtask.html", title="Task Manager - Add Task", selectedDate=today)
 
 @pages.post("/complete")
 def complete():
     dateString = request.form.get("date")
-    task = request.form.get("taskName")
-    someDate = date.fromisoformat(dateString)
-    completions[someDate].append(task)
+    task = request.form.get("taskId")
+    date = datetime.fromisoformat(dateString)
+    current_app.db.completions.insert_one({"date": date, "task": task})
+    # completions[someDate].append(task)
 
     return redirect(url_for("tasks.index", date=dateString))
